c5319c452951a070f26eaecf6cdd61ee57250fa7
MICHAELPROPHETE <MICHAELPROPHETE@HOTMAIL.COM>
2022-08-16 19:47:48 -0400

    Update k3s-clone-script.sh

--- k3s-clone-script.sh
+++ k3s-clone-script.sh
@@ -2,23 +2,23 @@
 
 #k3s-01
 sudo qm clone 10000 401 --full --storage villager-1 --name k3s-01
-sudo qm set 401 --net0 virtio,bridge=vmbr0,macaddr=ba:ba:f9:4f:0e:b3
+sudo qm set 401 --net0 virtio,bridge=vmbr0,macaddr=<my vm mac address>
 vzdump 401
 #k3s-02
 sudo qm clone 10000 402 --full --storage villager-1 --name k3s-02
-sudo qm set 402 --net0 virtio,bridge=vmbr0,macaddr=fa:f7:48:99:5a:5c
+sudo qm set 402 --net0 virtio,bridge=vmbr0,macaddr=<my vm mac address>
 vzdump 402
 #k3s-03
 sudo qm clone 10000 403 --full --storage villager-1 --name k3s-03
-sudo qm set 403 --net0 virtio,bridge=vmbr0,macaddr=62:30:3f:87:4c:0e
+sudo qm set 403 --net0 virtio,bridge=vmbr0,macaddr=<my vm mac address>
 vzdump 403
 #k3s-04
 sudo qm clone 10000 404 --full --storage villager-1 --name k3s-04
-sudo qm set 404 --net0 virtio,bridge=vmbr0,macaddr=46:2a:ca:da:ae:02
+sudo qm set 404 --net0 virtio,bridge=vmbr0,macaddr=<my vm mac address>
 vzdump 404
 #k3s-05
 sudo qm clone 10000 405 --full --storage villager-1 --name k3s-05
-sudo qm set 405 --net0 virtio,bridge=vmbr0,macaddr=b2:39:3f:55:35:8c
+sudo qm set 405 --net0 virtio,bridge=vmbr0,macaddr=<my vm mac address>
 vzdump 405
 
 clear
