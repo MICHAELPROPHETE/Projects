c939312644e58d58218d8b3215f9c133b836f950
MICHAELPROPHETE <MICHAELPROPHETE@HOTMAIL.COM>
2022-08-16 20:04:36 -0400

    Create k3s-start-script.sh

---
+++ k3s-start-script.sh
@@ -0,0 +1,16 @@
+#k3s Clone Start Script
+
+#k3s-01
+qm start 401
+#k3s-02
+qm start 402
+#k3s-03
+qm start 403
+#k3s-04
+qm start 404
+#k3s-05
+qm start 405
+
+clear
+
+echo "the vm's are ready to go"
