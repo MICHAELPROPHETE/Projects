c31aa2c24ca2ed4127233fb94c01c05c8c61eace
MICHAELPROPHETE <MICHAELPROPHETE@HOTMAIL.COM>
2022-08-16 20:02:39 -0400

    Create k3s-clone-destroy-script.sh

---
+++ k3s-clone-destroy-script.sh
@@ -0,0 +1,21 @@
+#k3s Clone Detroy Script
+
+#k3s-01
+sudo qm destroy 401 --purge
+
+#k3s-02
+sudo qm destroy 402 --purge
+
+#k3s-03
+sudo qm destroy 403 --purge
+
+#k3s-04
+sudo qm destroy 404 --purge
+
+#k3s-05
+sudo qm destroy 405 --purge
+
+
+clear
+
+echo "the vm's are deleted as well as their backups"
